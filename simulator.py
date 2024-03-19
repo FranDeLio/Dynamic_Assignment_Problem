2a55bd42a07e5da369f46136ed87820d385cd727
FranDeLio <franciscodelioperego@gmail.com>
2024-03-19 03:04:51 +0100

    Add comments.

--- simulator.py
+++ simulator.py
@@ -15,6 +15,7 @@ import pandas as pd
 n_served_customers = 0
 
 def timeit(func):
+    """Decorator to measure the execution time of a function."""
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):
         start_time = time.perf_counter()
@@ -27,8 +28,8 @@ def timeit(func):
 
     return timeit_wrapper
 
-
 class Car(simpy.Resource):
+    """Representation of a car in the simulation."""
     def __init__(self, env, id, map_size, capacity):
         self.id = id
         self.map_size = map_size
@@ -39,6 +40,7 @@ class Car(simpy.Resource):
         super().__init__(env, capacity)
 
 class User:
+    """Representation of a user in the simulation."""
     def __init__(self, env, id, map_size):
         self.env = env
         self.id = id
@@ -51,32 +53,27 @@ class User:
         self.waiting_time = 0
 
     def use_car(self, car):
-        # User gets a car and drives for a random amount of time
+        """User action to use a car."""
         print(f"User {self.id} takes car {car.id} at time {self.env.now}")
         with car.request() as request:
 
             service_request_time = self.env.now
-
             yield request
-
             self.waiting_time = self.env.now - service_request_time
-
             service_start_time = self.env.now
-        
             yield self.env.timeout(5)
-
             self.service_time = self.env.now - service_start_time
 
         # User returns the car
         print(f"User {self.id} leaves car {car.id} at time {self.env.now}")
 
     def _use_car(self, car):
-
         self.waiting_time = 69
         self.service_time = 7
 
 
 class Dispatcher:
+    """Dispatcher class to manage car-user assignments."""
     def __init__(self, env, users, available_cars, solver):
         self.env = env
         self.solver = solver
@@ -85,50 +82,45 @@ class Dispatcher:
         self.simulation_data = []
 
     def solve(self, solver) -> float:
+        """Solve the assignment problem using the specified solver."""
         optimal_matching = solver.get_optimal_solution(self.available_cars, self.users)
         return optimal_matching
 
     def add_new_user(self, user):
+        """Add a new user to the simulation."""
         self.users.update({user.id: user})
 
     def collect_service_data(self, user, car):
-        x={"user_id": user.id, "car_id": car.id, "service_time": user.service_time, "waiting_time": user.waiting_time}
-        print(x)
-        self.simulation_data.append(x)
+        """Collect service data for analysis."""
+        data={"user_id": user.id, "car_id": car.id, "service_time": user.service_time, "waiting_time": user.waiting_time}
+        print(data)
+        self.simulation_data.append(data)
 
     def dispatch(self):
-        # Generate all possible combinations of users and cars
+        """Dispatch cars to users."""
         global n_served_customers
 
-        # Find the assignment that minimizes total distance
-        if (len(self.available_cars) == 0) | (len(self.users) == 0):
-            pass
-        else:
-            optimal_solution = self.solve(self.solver)
-
-            # Assign cars to users based on the globally optimal assignment
-            for user_id, car_id in optimal_solution.items():
-                
-                car=self.available_cars.get(car_id)
-                user=self.users.get(user_id)
-                print((user, car))
-
-                self.available_cars.pop(car_id, None)
-
-                self.env.process(user.use_car(car))
-                user._use_car(car)
-
-                self.available_cars.update({car_id: car})
-                car.position = user.position
-                
-                self.collect_service_data(user, car)
-                print(f"fff {user.waiting_time}")
-
-                self.users.pop(user_id, None)
-
-                n_served_customers += 1
+        if not self.available_cars or not self.users:
+            return
+        
+        optimal_solution = self.solve(self.solver)
+        # Assign cars to users based on the globally optimal assignment
+        for user_id, car_id in optimal_solution.items():
+            
+            car=self.available_cars.get(car_id)
+            user=self.users.get(user_id)
+            self.available_cars.pop(car_id, None)
+            self.env.process(user.use_car(car))
+            #user._use_car(car)
+            self.available_cars.update({car_id: car})
+            car.position = user.position
+            self.collect_service_data(user, car)
+            print(f"fff {user.waiting_time}")
+            self.users.pop(user_id, None)
+            n_served_customers += 1
 
 class CarSharingSimulation:
+    """Simulation of car sharing system."""
     def __init__(self, env, num_cars, num_users, map_size, simulation_time):
         self.env = env
         self.num_cars = num_cars
@@ -142,14 +134,14 @@ class CarSharingSimulation:
 
     @timeit
     def run(self):
+        """Run the simulation."""
         # Schedule the dispatcher to run every 1 minute
         self.env.process(self.dispatch_cars())
-
         # Start the simulation
         self.env.run(until=self.simulation_time) # Run the simulation for 50 time units
 
     def dispatch_cars(self):
-
+        """Dispatch cars at regular intervals."""
         while True:
             self.dispatcher.dispatch()
             n_new_users = np.random.poisson(1)
@@ -162,6 +154,7 @@ class CarSharingSimulation:
             yield self.env.timeout(15)  # Dispatch cars every 1 minute
 
     def get_simulation_data(self):
+        """Get simulation data for analysis."""
         return self.dispatcher.simulation_data 
 
 
