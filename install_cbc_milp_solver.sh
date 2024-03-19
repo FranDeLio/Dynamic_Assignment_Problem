80ba3df37d3cc6c94163fb3236387958a99e4b28
FranDeLio <franciscodelioperego@gmail.com>
2024-03-19 18:29:26 +0100

    Separate cbc intaller from venv setup.

---
+++ install_cbc_milp_solver.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo apt-get install  coinor-cbc coinor-libcbc-dev
--- set_up_environment.sh
+++ set_up_environment.sh
@@ -3,4 +3,3 @@
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-sudo apt-get install  coinor-cbc coinor-libcbc-dev
