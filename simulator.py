c6f5125df12ad0fd7b6463df2b72e6af3c669a74
FranDeLio <franciscodelioperego@gmail.com>
2024-03-23 12:28:02 +0100

    Proper data handling in the simulator. Though an edge case exists where orders were assigned but not delivered.

--- simulator.py
+++ simulator.py
@@ -25,10 +25,9 @@ class Car(simpy.Resource):
 
 class User:
     """Representation of a user in the simulation."""
-    def __init__(self, env, id, map_size, simulation_id):
+    def __init__(self, env, id, map_size):
         self.env = env
         self.id = id
-        self.simulation_id = simulation_id
         self.map_size = map_size
         self.coordinates_start = (
             random.uniform(0, self.map_size[0]),
@@ -39,6 +38,17 @@ class User:
             random.uniform(0, self.map_size[1]),
         )
         self.request_activation_time = self.env.now
+        self.simulation_id = simulation_id
+        self.served_status = False
+        self.service_start_time = None
+        self.waiting_time = np.nan
+        self.service_start_time = None
+        self.request_completion_time = None
+        self.service_time = np.nan
+        self.order_fullfilment_time = np.nan
+
+    def to_dict(self):
+        return vars(self)
 
     def save_assignment_cost(self, assignment_cost):
         self.assignment_cost = round(assignment_cost, ndigits=3)
@@ -46,29 +56,21 @@ class User:
     def use_car(self, car):
         """User action to use a car."""
         print(f"User {self.id} takes car {car.id} at time {self.env.now}")
-        global simulation_data
+        self.car_id = car.id
         with car.request() as request:
 
             yield request
             #note that start to customer and customer to destination are not split in this logic
-            waiting_time = round(self.env.now - self.request_activation_time, 2)
-            service_start_time = round(self.env.now, 2)
+            self.waiting_time = round(self.env.now - self.request_activation_time, 2)
+            self.service_start_time = round(self.env.now, 2)
             yield self.env.timeout(self.assignment_cost)
-            request_completion_time = round(self.env.now, 2)
-            service_time = round(request_completion_time - service_start_time, 2)
-            order_fullfilment_time = round(request_completion_time - self.request_activation_time, 2)
-        
-        request_data={"user_id": self.id, "car_id": car.id, "activation_time": self.request_activation_time,
-              "request_completion_time": request_completion_time,"waiting_time": waiting_time,
-              "service_time": service_time, "order_fullfilment_time": order_fullfilment_time, 
-              "assignment_cost": self.assignment_cost, "simulation_id": self.simulation_id}
-        simulation_data.append(request_data)
+            self.request_completion_time = round(self.env.now, 2)
+            self.service_time = round(self.request_completion_time - self.service_start_time, 2)
+            self.order_fullfilment_time = round(self.request_completion_time - self.request_activation_time, 2)
+            self.served_status = True
         
         # User returns the car
         print(f"User {self.id} leaves car {car.id} at time {self.env.now}")
-
-        return waiting_time
-
 
 class Dispatcher:
     """Dispatcher class to manage car-user assignments."""
@@ -76,6 +78,7 @@ class Dispatcher:
         self.env = env
         self.solver = solver
         self.users = {user.id: user for user in users}
+        self.served_users = []
         self.available_cars = {car.id: car for car in available_cars}
         self.simulation_data = []
         self.n_served_customers = 0
@@ -90,9 +93,10 @@ class Dispatcher:
         """Add a new user to the simulation."""
         self.users.update({user.id: user})
 
-    def remove_served_user(self, user):
+    def set_user_to_served(self, user):
         """Remove user from the simulation."""
         self.users.pop(user.id, None)
+        self.served_users.append(user)
 
     def set_car_to_busy(self, car):
         """Remove car from set of availabile cars."""
@@ -121,7 +125,7 @@ class Dispatcher:
             self.env.process(user.use_car(car))
             self.move_car_to_ride_destination(car, user)
             self.n_served_customers += 1
-            self.remove_served_user(user)
+            self.set_user_to_served(user)
             self.set_car_to_free(car)
             
 
@@ -143,11 +147,24 @@ class RideSharingSimulation:
         self.num_users = num_users
         self.map_size = map_size
         self.cars = [Car(env, i, self.map_size, capacity=1) for i in range(num_cars)]
-        self.users = [User(env, i, self.map_size, self.id) for i in range(num_users)]
+        self.users = [User(env, i, self.map_size) for i in range(num_users)]
         self.available_cars = self.cars  # Initially, all cars are available
         self.dispatcher = Dispatcher(env, self.users, self.cars, MILPSolver(decision_variable_type="real", solver_name="cbc"))
         self.simulation_time = simulation_time
         self.expected_customer_inflow = inflow_rate
+
+    def dispatch_cars(self):
+        """Dispatch cars at regular intervals."""
+        while True:
+            self.dispatcher.dispatch()
+            n_new_users = np.random.poisson(self.expected_customer_inflow)
+
+            for _ in range(0, n_new_users):
+                self.dispatcher.add_new_user(User(env, self.num_users, self.map_size))
+                print(f"User {self.num_users} joins the simulation at time {env.now}")
+                self.num_users += 1
+
+            yield self.env.timeout(15)  # Dispatch cars every 1 minute
 
     @timeit
     def run(self):
@@ -157,32 +174,40 @@ class RideSharingSimulation:
         # Start the simulation
         self.env.run(until=self.simulation_time) # Run the simulation for 50 time units
 
-    def dispatch_cars(self):
-        """Dispatch cars at regular intervals."""
-        while True:
-            self.dispatcher.dispatch()
-            n_new_users = np.random.poisson(self.expected_customer_inflow)
+    def load_simulation_data(self):
 
-            for _ in range(0, n_new_users):
-                self.dispatcher.add_new_user(User(env, self.num_users, self.map_size, self.id))
-                print(f"User {self.num_users} joins the simulation at time {env.now}")
-                self.num_users += 1
+        self.simulation_data = []
+        self.average_waiting_time = np.nanmean([user.waiting_time for user in self.dispatcher.served_users])
+        self.average_service_time = np.nanmean([user.service_time for user in self.dispatcher.served_users])
+        self.average_order_fullfilment_time = np.nanmean([user.order_fullfilment_time for user in self.dispatcher.served_users])
+        self.average_assignment_cost = np.nanmean([user.assignment_cost for user in self.dispatcher.served_users])
+        self.n_served_users = len(self.dispatcher.served_users)
 
-            yield self.env.timeout(15)  # Dispatch cars every 1 minute
+        for user in self.dispatcher.served_users:
+            
+            try:
+                user_data={"user_id": user.id, "car_id": user.car_id, "activation_time": user.request_activation_time,
+                    "request_completion_time": user.request_completion_time,"waiting_time": user.waiting_time,
+                    "service_time": user.service_time, "order_fullfilment_time": user.order_fullfilment_time, 
+                    "assignment_cost": user.assignment_cost, "simulation_id": self.id}
+            
+                self.simulation_data.append(user_data)
+            except Exception as error:
+                print(error)
+                print(user.to_dict())
 
-    def get_global_cost(self):
-        """Get simulation data for analysis."""
-        return round(self.dispatcher.global_assignment_cost/self.dispatcher.n_served_customers, ndigits=2)
 
 # Setup and run the simulation
 simulation_results = {"simulation_id": [], "global_cost": []}
-    
-for simulation_id in range(0, 10): 
+
+for simulation_id in range(0, 1): 
     env = simpy.Environment()
     simulation = RideSharingSimulation(env=env, id=simulation_id, num_cars=20, num_users=40, map_size=(10, 10), simulation_time=200, inflow_rate=10)
     simulation.run()
-    simulation_results.get("simulation_id").append(simulation_id)
-    simulation_results.get("global_cost").append(simulation.get_global_cost())
+    simulation.load_simulation_data()
+    simulation_data = simulation.simulation_data
  
 simulation_data = pd.DataFrame(simulation_data).to_csv("sim_data.csv", index=False)
-simulation_results = pd.DataFrame(simulation_results).to_csv("sim_results.csv", index=False)
+print(simulation_data)
+print(f"Service time: {simulation.average_service_time}")
+print(f"Waiting time: {simulation.average_waiting_time}")
