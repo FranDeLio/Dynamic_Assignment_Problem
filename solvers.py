32b69044ac47d95ca82bc8c9902e920874fafc07
FranDeLio <franciscodelioperego@gmail.com>
2024-03-25 16:17:57 +0100

    Minor naming changes.

--- solvers.py
+++ solvers.py
@@ -14,11 +14,10 @@ logging.getLogger("pyomo.core").setLevel(logging.ERROR)
 
 class AssignmentSolver(ABC):
     #this is a good place to specify the objective function to be used
-
     @abstractmethod
     def get_optimal_solution(self) -> float:
         pass
-
+    
 
 class MILPSolver(AssignmentSolver):
     def __init__(
@@ -36,10 +35,10 @@ class MILPSolver(AssignmentSolver):
         else:
             decision_domain = pe.Binary
 
-        self.source_coordinates = [car.coordinates_start for car in available_cars.values()]
+        self.car_coordinates = [car.coordinates_start for car in available_cars.values()]
         self.destination_coordinates = [user.coordinates_start for user in users.values()]
         self.cost_matrix = distance.cdist(
-            self.source_coordinates, self.destination_coordinates, "euclidean"
+            self.car_coordinates, self.destination_coordinates, "euclidean"
         )
 
         self.available_cars_ids = [car.id for car in available_cars.values()]
@@ -49,43 +48,43 @@ class MILPSolver(AssignmentSolver):
         solver = po.SolverFactory(self.solver_name)
         model = pe.ConcreteModel()
 
-        model.source = pe.Set(initialize=self.available_cars_ids)
-        model.destination = pe.Set(initialize=self.users_ids)
+        model.cars = pe.Set(initialize=self.available_cars_ids)
+        model.users = pe.Set(initialize=self.users_ids)
 
-        cost = (pd.DataFrame(self.cost_matrix, index=self.available_cars_ids, columns=self.users_ids)
+        assignment_cost = (pd.DataFrame(self.cost_matrix, index=self.available_cars_ids, columns=self.users_ids)
                 .stack()
                 .to_dict())
         
-        model.cost = pe.Param(model.source, model.destination, initialize=cost)
+        model.assignment_cost = pe.Param(model.cars, model.users, initialize=assignment_cost)
         model.y = pe.Var(
-            model.source, model.destination, domain=decision_domain
+            model.cars, model.users, domain=decision_domain
         )  # variable: 1 if assign parameters set k to city c else 0.
 
         expression = sum(
-            model.cost[c, k] * model.y[c, k]
-            for c in model.source
-            for k in model.destination
+            model.assignment_cost[c, k] * model.y[c, k]
+            for c in model.cars
+            for k in model.users
         )
         model.obj = pe.Objective(sense=pe.minimize, expr=expression)
 
-        def serve_all_destinations(model, k):
+        def each_car_serves_one_user_max(model, k):
             # assign exactly one origin set c to every destination k.
-            constraint = sum(model.y[c, k] for c in model.source) <= 1
+            constraint = sum(model.y[c, k] for c in model.cars) <= 1
             return constraint
 
-        def origin_unicity(model, c):
+        def each_user_is_served_by_one_car_max(model, c):
             # an origin c can only be assign to one given destination k.
-            constraint = sum(model.y[c, k] for k in model.destination) <= 1
+            constraint = sum(model.y[c, k] for k in model.users) <= 1
             return constraint
         
-        def min_assignments(model):
+        def maximal_injective_assignment(model):
             # a minimum of min(|origin|,|destination|) matches ought to be made.
-            constraint = sum(model.y[c, k] for k in model.destination for c in model.source) == self.n_assignments
+            constraint = sum(model.y[c, k] for k in model.users for c in model.cars) == self.n_assignments
             return constraint
 
-        model.serve_all_destinations = pe.Constraint(model.destination, rule=serve_all_destinations)
-        model.origin_unicity = pe.Constraint(model.source, rule=origin_unicity)
-        model.min_assignments = pe.Constraint(rule=min_assignments)
+        model.each_car_serves_one_user_max = pe.Constraint(model.users, rule=each_car_serves_one_user_max)
+        model.each_user_is_served_by_one_car_max = pe.Constraint(model.cars, rule=each_user_is_served_by_one_car_max)
+        model.maximal_injective_assignment = pe.Constraint(rule=maximal_injective_assignment)
 
         result = solver.solve(model, timelimit=60)
 
@@ -98,7 +97,7 @@ class MILPSolver(AssignmentSolver):
         n_rows = solution_df.shape[0]
 
         for i in range(0, n_rows):
-           solution_df.loc[i,"assignment_cost"] = cost[solution_df.loc[i,"cars"], solution_df.loc[i,"users"]]
+           solution_df.loc[i,"assignment_cost"] = assignment_cost[solution_df.loc[i,"cars"], solution_df.loc[i,"users"]]
         
         result = solution_df.to_dict(orient='records')
         
